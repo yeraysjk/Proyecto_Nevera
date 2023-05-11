26ce73575b28a514508bbec521b4454c42cc8088
Yeray <yeraysjkk@gmail.com>
2023-05-11 17:42:40 +0200

    2

---
+++ src/main/java/yeray/priede/proyecto_nevera/Producte.java
@@ -0,0 +1,46 @@
+package yeray.priede.proyecto_nevera;
+
+public class Producte {
+    String nom;
+    String descripcio;
+    int preu;
+
+    public Producte(String nom, String descripcio, int preu) {
+        this.nom = nom;
+        this.descripcio = descripcio;
+        this.preu = preu;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setDescripcio(String descripcio) {
+        this.descripcio = descripcio;
+    }
+
+    public void setPreu(int preu) {
+        this.preu = preu;
+    }
+
+    public String getDescripcio() {
+        return descripcio;
+    }
+
+    public int getPreu() {
+        return preu;
+    }
+
+    public Producte() {
+    }
+
+
+    public String afegirProducte(String nom,String descripcio, int preu,){
+
+
+    }
+}
